027efa755ca13ec12fdcdcb623423d679d00703e
nom <guillaume.pham@ynov.com>
2022-02-04 14:41:08 +0100

    Faut bouger les css de mort

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
-function jacque (i){
+let clique_sur_moi = Document.getElementById("clique_sur_moi");
 
-}
+let wiki = Document.getElementById("wiki");
+clique_sur_moi.addEventListener("click", () => {
+  if(getComputedStyle(wiki).display != "none"){
+    wiki.style.display = "none";
+  } else {
+    wiki.style.display = "block";
+  }
+})
