0cdca450da39cb3d254dac4a40078a01fd8cb829
nom <guillaume.pham@ynov.com>
2022-02-07 20:03:07 +0100

    Boss css et debug

--- js/script.js
+++ js/script.js
@@ -1,47 +1,29 @@
-let clique_sur_moi = Document.getElementById("clique_sur_moi");
+let counter = 0;
+let bosses = [document.getElementById("dimitrescu"), document.getElementById("beneviento"), document.getElementById("morreau"), document.getElementById("heisenberg"), document.getElementById("miranda"),]
 
-let wiki = Document.getElementById("wiki");
-clique_sur_moi.addEventListener("click", () => {
-  if(getComputedStyle(wiki).display != "none"){
-    wiki.style.display = "none";
-  } else {
-    wiki.style.display = "block";
-  }
-})
-
-
-let exemple = document.getElementById("soluce");
-exemple.classList.add("none");
-
-function Solution(){
-  exemple.classList.toggle("none");
+function leftButton(){
+    counter--;
+    if (counter < 0) {
+        counter = 4
+    }
+    for (let index = 0; index < bosses.length; index++) {
+        const element = bosses[index];
+        element.classList.remove("display")
+        if (counter == index) {
+            element.classList.add("display")
+        }
+    }
 }
-
-  let counter = 0;
-  let bosses = [document.getElementById("dimitrescu"), document.getElementById("beneviento"), document.getElementById("morreau"), document.getElementById("heisenberg"), document.getElementById("miranda"),]
-  function leftButton(){
-      counter--;
-      if (counter < 0) {
-          counter = 4
-      }
-      for (let index = 0; index < bosses.length; index++) {
-          const element = bosses[index];
-          element.classList.remove("display")
-          if (counter == index) {
-              element.classList.add("display")
-          }
-      }
-  }
-  function rightButton(){
-      counter++;
-      if (counter > 4) {
-          counter = 0
-      }
-      for (let index = 0; index < bosses.length; index++) {
-          const element = bosses[index];
-          element.classList.remove("display")
-          if (counter == index) {
-              element.classList.add("display")
-          }
-      }
-  }
+function rightButton(){
+    counter++;
+    if (counter > 4) {
+        counter = 0
+    }
+    for (let index = 0; index < bosses.length; index++) {
+        const element = bosses[index];
+        element.classList.remove("display")
+        if (counter == index) {
+            element.classList.add("display")
+        }
+    }
+}
